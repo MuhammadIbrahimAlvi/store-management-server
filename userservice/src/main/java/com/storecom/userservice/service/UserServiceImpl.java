091179424ff8e7da7da5d3ee01064ca67654d236
Muhammad Ibrahim Alvi <ebrahimalvi2@gmail.com>
2023-10-14 22:55:38 +0500

    main created crud operations for the user.

--- userservice/src/main/java/com/storecom/userservice/service/UserServiceImpl.java
+++ userservice/src/main/java/com/storecom/userservice/service/UserServiceImpl.java
@@ -1,7 +1,40 @@
 package com.storecom.userservice.service;
 
+import com.storecom.userservice.entity.User;
+import com.storecom.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        if (userRepository.existsById(id)) {
+            updatedUser.setId(id);
+            return userRepository.save(updatedUser);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
