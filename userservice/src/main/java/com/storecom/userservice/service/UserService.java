4f3125e9ce5ae1861796b45941df937f1d1b99d6
Muhammad Ibrahim Alvi <ebrahimalvi2@gmail.com>
2023-10-14 23:20:02 +0500

    main added UserService interface.

--- userservice/src/main/java/com/storecom/userservice/service/UserService.java
+++ userservice/src/main/java/com/storecom/userservice/service/UserService.java
@@ -1,4 +1,18 @@
 package com.storecom.userservice.service;
 
+import com.storecom.userservice.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    List<User> getAllUsers();
+
+    Optional<User> getUserById(Long id);
+
+    User createUser(User user);
+
+    User updateUser(Long id, User updatedUser);
+
+    void deleteUser(Long id);
 }
