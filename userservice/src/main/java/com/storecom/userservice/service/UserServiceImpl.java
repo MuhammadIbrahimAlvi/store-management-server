c232fd729585d180c05b470330901ddc9bbd8d06
Muhammad Ibrahim Alvi <ebrahimalvi2@gmail.com>
2023-11-09 21:55:56 +0500

    main Import Optimize.

--- userservice/src/main/java/com/storecom/userservice/service/UserServiceImpl.java
+++ userservice/src/main/java/com/storecom/userservice/service/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.storecom.userservice.repository.UserRepository;
 import com.storecom.userservice.util.ConversionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
